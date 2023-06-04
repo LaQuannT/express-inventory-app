c97cb5caf50de0420c05244a004c51923ebf4e2a
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-06-04 10:31:09 +0100

    build: add schemas for sneakers & brands

---
+++ sneaker.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const SneakerSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  colorway: { type: String, required: true },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand" },
+  price: { type: Number, required: true },
+  pairs: { type: Number, min: 1, required: true },
+});
+
+SneakerSchema.virtual("url").get(function () {
+  return `/sneaker/${this._id}`;
+});
+
+module.exports = mongoose.model("Sneaker", SneakerSchema);
