e4da4242d2f76fc1c511d01006d712fa8c6f2d12
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-06-05 08:55:12 +0100

    feat: add route to brand list view

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,7 +3,9 @@ const Brand = require("../models/brand");
 const asyncHandler = require("express-async-handler");
 
 exports.brand_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLENTED: Brand List");
+  const brandList = await Brand.find({}, "name").sort({ name: 1 }).exec();
+
+  res.render("brand_list", { title: "All Brands", brandList });
 });
 
 exports.brand_details = asyncHandler(async (req, res, next) => {
--- routes/collection.js
+++ routes/collection.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const sneaker_controller = require("../controllers/sneakerController");
+const brand_controller = require("../controllers/brandController");
 
 router.get("/", sneaker_controller.index);
 
 router.get("/sneaker/all", sneaker_controller.sneaker_list);
+
+router.get("/brand/all", brand_controller.brand_list);
 
 module.exports = router;
