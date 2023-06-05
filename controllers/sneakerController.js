49c15c19fe74092f95a11aba8622ec6c2f6f311f
LaQuan <117905467+LaQuannT@users.noreply.github.com>
2023-06-05 18:22:00 +0100

    feat: update route to sneaker create

--- controllers/sneakerController.js
+++ controllers/sneakerController.js
@@ -2,6 +2,7 @@ const Sneaker = require("../models/sneaker");
 const Brand = require("../models/brand");
 
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
   const [numSneakers, numBrands] = await Promise.all([
@@ -30,12 +31,59 @@ exports.sneaker_details = asyncHandler(async (req, res, next) => {
 });
 
 exports.sneaker_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLENTED: Sneaker create get");
+  const allBrands = await Brand.find().sort({ name: 1 }).exec();
+  res.render("sneaker_form", {
+    title: "Add Sneaker",
+    brands: allBrands,
+  });
 });
 
-exports.sneaker_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLENTED: Sneaker create post");
-});
+exports.sneaker_create_post = [
+  body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("colorway", "Coloway must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("brand", "Brand must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must be a value with 2 decimal places.")
+    .isFloat({ min: 0.0 })
+    .escape(),
+  body("pairs", "Must contain at least 1 pair.").isNumeric().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const sneaker = new Sneaker({
+      name: req.body.name,
+      description: req.body.description,
+      colorway: req.body.colorway,
+      brand: req.body.brand,
+      price: req.body.price,
+      pairs: req.body.pairs,
+    });
+
+    if (!errors.isEmpty()) {
+      const validBrands = await Brand.find().exec();
+
+      res.render("sneaker_form", {
+        title: "Add Sneaker",
+        brands: validBrands,
+        sneaker,
+        errors: errors.array(),
+      });
+    } else {
+      await sneaker.save();
+      res.redirect(sneaker.url);
+    }
+  }),
+];
 
 exports.sneaker_delete_get = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLENTED: Sneaker delete get");
